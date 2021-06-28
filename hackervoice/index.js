0c2b8f7b0e631abb52ba935bc517913934ab1098
Muctar Sanneh <50896430+muctars@users.noreply.github.com>
2021-06-28 14:45:40 -0700

    added hackervoice function

---
+++ hackervoice/index.js
@@ -0,0 +1,15 @@
+// template code whenever you create an HTTP trigger function
+// this is getting something from a parameter and outputting it to the user
+
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    const password = req.query.password || req.body.password;
+
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: password
+    };
+
+}
+
