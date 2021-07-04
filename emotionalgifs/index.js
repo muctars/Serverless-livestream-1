ab936cc7c4c7dc8b19ea79432f6b2eda39a7422a
Muctar Sanneh <50896430+muctars@users.noreply.github.com>
2021-07-03 23:29:14 -0700

    updated emotionalgifs to return url associated with main emotion

--- emotionalgifs/index.js
+++ emotionalgifs/index.js
@@ -24,13 +24,31 @@ module.exports = async function (context, req) {
     
     const main_emotion = Object.keys(emotions).find(key => emotions[key] === Math.max(...objects));
 
-
+    let gif = await getGif(main_emotion);
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: main_emotion
+        body: gif.data.url
     };
 }
 
+async function getGif(emotion) {
+    const giphyKey = process.env.GIPHY_KEY;
+    const giphyEndpoint = "https://api.giphy.com/v1/gifs/translate";
+
+    let params = new URLSearchParams({
+        'api_key': giphyKey,
+        's': emotion     //FILL IN THIS LINE
+    })
+
+    let resp = await fetch(giphyEndpoint  + '?' + params.toString(), {
+        method: "GET"
+
+        }
+    )
+
+    const giphyResult = await resp.json();
+    return giphyResult;
+}
 async function analyzeImage(img){
     const subscriptionKey = process.env.FACEAPI_KEY1;
     const uriBase = process.env.FACEAPI_ENDPOINT + '/face/v1.0/detect';
