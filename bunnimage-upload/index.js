9f68ee8506ae00592b3daf818c66271f2a825c65
Muctar Sanneh <50896430+muctars@users.noreply.github.com>
2021-07-09 10:50:39 -0700

    updated bunnimage

--- bunnimage-upload/index.js
+++ bunnimage-upload/index.js
@@ -5,40 +5,50 @@ const { BlobServiceClient } = require("@azure/storage-blob");
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    let boundary = multipart.getBoundary(req.headers['content-type']);
-    let body = req.body;
-    let parsedBody = multipart.Parse(body, boundary);
+
+    let responseMessage = "";
+
+    try{
+        let password = req.headers['codename'];; // this will be the codename
 
 
-    let filetype = parsedBody[0].type;
-    if (filetype == "image/png") {
-        ext = "png";
-    } else if (filetype == "image/jpeg") {
-        ext = "jpeg";
-    } else if (filetype == "image/jpg") {
-        ext = "jpg"
-    } else {
-        username = "invalidimage"
-        ext = "";
+        let boundary = multipart.getBoundary(req.headers['content-type']);
+        let body = req.body;
+        let parsedBody = multipart.Parse(body, boundary);
+
+        let filetype = parsedBody[0].type;
+        if (filetype == "image/png") {
+            ext = "png";
+        } else if (filetype == "image/jpeg") {
+            ext = "jpeg";
+        } else if (filetype == "image/jpg") {
+            ext = "jpg"
+        } else {
+            username = "invalidimage"
+            ext = "";
+        }
+
+        responseMessage = await uploadFile(parsedBody, ext, password);
+    } catch(err){
+        context.log("Undefined body image")
+        responseMessage = "Sorry! No image attached";
     }
-
-    let responseMessage = await uploadFile(parsedBody, ext);
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: responseMessage
     };
 }
 
-async function uploadFile(parsedBody, ext){
+async function uploadFile(parsedBody, ext, password){
     // we want to access a specific container in azure, and we are giving our credentials
     const blobServiceClient = BlobServiceClient.fromConnectionString(connectionString); //
     const containerName = "images";
     const containerClient = blobServiceClient.getContainerClient(containerName);    // Get a reference to a container
 
-    const blobName = 'test.' + ext;    // Create the container
+    const blobName = password + "." + ext;    // Create the container
     const blockBlobClient = containerClient.getBlockBlobClient(blobName); // Get a block blob client
 
     const uploadBlobResponse = await blockBlobClient.upload(parsedBody[0].data, parsedBody[0].data.length);
 
-    return "Filed saved";
+    return "Your file has been saved";
 }
