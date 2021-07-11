596d17c2de56a6c0048b3f967965a3f6fb8dab5b
Muctar Sanneh <50896430+muctars@users.noreply.github.com>
2021-07-10 17:20:05 -0700

    updated deepsecrets

--- deepsecrets/index.js
+++ deepsecrets/index.js
@@ -19,8 +19,8 @@ module.exports = async function (context, req) {
     let message = queryObject.Body;
     let document = {"message" : message};
     let items = await createDocument(document);
-
-    const responseMessage = `Thanks 😊! Stored your secret "${message}". 😯 Someone confessed that: ${JSON.stringify(items[0].message)}`
+    const random_value = Math.floor(items.length * Math.random());
+    const responseMessage = `Thanks 😊! Stored your secret "${message}". 😯 Someone confessed that: ${JSON.stringify(items[random_value].message)}`
 
     context.res = {
         // status: 200, /* Defaults to 200 */
@@ -56,7 +56,7 @@ async function createDocument(newItem){
     await create(client, databaseId, containerId);
 
     const querySpec = {
-        query: "SELECT top 1 * FROM c order by c._ts desc"
+        query: "SELECT * from c"
     };
     
     const { resources: items } = await container.items.query(querySpec).fetchAll();
