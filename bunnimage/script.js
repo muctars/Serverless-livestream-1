b2b7ae153029e6a0f9c8456a85be625ee347ab91
Muctar Sanneh <50896430+muctars@users.noreply.github.com>
2021-07-21 16:26:49 -0700

    updated script.js file

--- bunnimage/script.js
+++ bunnimage/script.js
@@ -1,9 +1,43 @@
 function getImage (event) {
     event.preventDefault();
+    let bunniForm = document.getElementById("myform");
+    // get image upload by user via the form, saved in payload
     
+    let nameInput = document.getElementById('name');
+    let fileInput = document.getElementById('image');
+
+    let payload = new FormData(bunniForm);
+    console.log(payload)
+    const file = fileInput.files[0]; // fileInput is the file upload input element
+    payload.append("file", file);
+    $('#output').text("Thanks!")
+
+    let url = "https://emotionalmultiparse.azurewebsites.net/api/bunnimage-upload"
     if (document.getElementById("name").value != "") {
-        $('#output').text("Thanks!")
+
+        try{
+            let url = "https://emotionalmultiparse.azurewebsites.net/api/bunnimage-upload"
+
+           
+            console.log("Image was uploaded, making POST request to Azure function");
+            // Create request to our azure functions
+            const response = fetch(url, {
+                method: "POST",
+                headers: {
+                    "codename": nameInput.value
+                },
+                body: payload
+        })
+        $('output').text("Your image has been stored successfully!");
+        } catch(err){
+            $("#output").text(err);
+        }
+
+        
     } else {
         alert("No name error.");
     }
+
+    
+    
 }
